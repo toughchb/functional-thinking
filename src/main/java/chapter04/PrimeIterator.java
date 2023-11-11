ed1546f4d7ada0e7977b26369aa395aef50ae33f
toughchb <ggang2000@gmail.com>
2023-11-11 18:08:07 +0900

    chapter04 코드 추가

---
+++ src/main/java/chapter04/PrimeIterator.java
@@ -0,0 +1,22 @@
+package chapter04;
+
+import java.util.Iterator;
+
+public class PrimeIterator implements Iterator {
+    private int lastPrime = 1;
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public Integer next() {
+        return lastPrime = Prime.nextPrimeFrom(lastPrime);
+    }
+
+    @Override
+    public void remove() {
+        throw new RuntimeException("Fundamental nature of the universe exception");
+    }
+}
